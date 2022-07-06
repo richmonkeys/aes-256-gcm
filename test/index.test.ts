8c6adaaa520df0744a05d6559f042e991c1fddba
Jeff Wong <jeff@richmonkeys.com>
2022-07-06 16:48:31 +0800

    test decrypt

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,37 +1,31 @@
 import { decrypt, encrypt } from '../src'
 
-test('encrypt `"string data"` with `"string password"`', () => {
-  expect(encrypt('string', 'string password')).toBeTruthy()
+const testPassword = 'string password'
+
+test(`encrypt "string data" with "${testPassword}"`, () => {
+  expect(decrypt(encrypt('string data', testPassword), testPassword)).toEqual('string data')
 })
 
-test('encrypt `123456` with `"string password"`', () => {
-  expect(encrypt(123.456, 'string password')).toBeTruthy()
+test(`encrypt 123456 with "${testPassword}"`, () => {
+  expect(decrypt(encrypt(123456, testPassword), testPassword)).toEqual(123456)
 })
 
-test('encrypt `123.456` with `"string password"`', () => {
-  expect(encrypt(12.345, 'string password')).toBeTruthy()
+test(`encrypt 123.456 with "${testPassword}"`, () => {
+  expect(decrypt(encrypt(123.456, testPassword), testPassword)).toEqual(123.456)
 })
 
-test('encrypt `1.2345678901234568e+29` with `"string password"`', () => {
-  expect(encrypt(1.2345678901234568e+29, 'string password')).toBeTruthy()
+test(`encrypt 1.2345678901234568e+29 with "${testPassword}"`, () => {
+  expect(decrypt(encrypt(1.2345678901234568e+29, testPassword), testPassword)).toEqual(1.2345678901234568e+29)
 })
 
-test('encrypt `{ js: "object" }` with `"string password"`', () => {
-  expect(encrypt({ js: "object" }, 'string password')).toBeTruthy()
+test(`encrypt { js: "object" } with "${testPassword}"`, () => {
+  expect(decrypt(encrypt({ js: "object" }, testPassword), testPassword)).toEqual({ js: "object" })
 })
 
 test('encrypt `{ "json": "object" }` with `"string password"`', () => {
-  expect(encrypt({ "json": "object" }, 'string password')).toBeTruthy()
+  expect(decrypt(encrypt({ "json": "object" }, testPassword), testPassword)).toEqual({ "json": "object" })
 })
 
 test('encrypt `[{ js: "object" }]` with `"string password"`', () => {
-  expect(encrypt([{ js: "object" }], 'string password')).toBeTruthy()
-})
-
-test('encrypt `[{ js: "object" }]` with `"string password"`, output `"hex"` encoding', () => {
-  expect(encrypt([{ js: "object" }], 'string password')).toBeTruthy()
-})
-
-test('decrypt `"qmj0nM9Jx2haDzVCUNKvaAL7VOBwpPL2hQz_-4W9-Z2fv4oV5lCtcvCnm47PtFiudv6Ijs6j-q2l1aO9KqhnCAWTdJta3BNsS2OAMgnPnWJEhKes0BG-WSPhjVN-iHciwLV5NVHtDzM"` with `"string password"`', () => {
-  expect(decrypt('qmj0nM9Jx2haDzVCUNKvaAL7VOBwpPL2hQz_-4W9-Z2fv4oV5lCtcvCnm47PtFiudv6Ijs6j-q2l1aO9KqhnCAWTdJta3BNsS2OAMgnPnWJEhKes0BG-WSPhjVN-iHciwLV5NVHtDzM', 'string password')).toBeTruthy()
+  expect(decrypt(encrypt([{ js: "object" }], testPassword), testPassword)).toEqual([{ js: "object" }])
 })
